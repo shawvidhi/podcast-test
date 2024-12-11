8d672a324161d60d95853dfa7af92b3cc667eb87
VIDHI SHAW <shawvidhi@gmail.com>
2024-12-11 00:26:44 +0000

    further changes

--- feed.py
+++ feed.py
@@ -15,8 +15,35 @@ rss_element = xml_tree.Element('rss', {
 # Create a <channel> element as a child of the <rss> element
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
+link_prefix = yaml_data['link']
+
 # Add a <title> element inside the <channel> and set its text to the title from the YAML data
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
+xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+xml_tree.SubElement(channel_element, 'link').text = link_prefix
+
+xml_tree.SubElement(channel_element, 'category').text = yaml_data['category']
+
+
+for item in yaml_data['item']:
+    item_element = xml_tree.SubElement(channel_element, 'item') 
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(item_element, 'description').text = item['description']
+    xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
+    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+        'url' : link_prefix + item['file'], 
+        'type' : 'audio/mpeg',
+        'length' : item['length']  
+    })
 
 # Create an ElementTree object with the root <rss> element
 output_tree = xml_tree.ElementTree(rss_element)
